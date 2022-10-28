2feca7bc83033db4b1750929cbf007edfb3cf327
MSlegro <legromanuel29@gmail.com>
2022-10-28 16:16:28 -0500

    Actualizacion Login, archivos y logica.

--- Datos/RepositorioProductos.cs
+++ Datos/RepositorioProductos.cs
@@ -8,7 +8,7 @@ using System.Threading.Tasks;
 
 namespace Datos
 {
-    public class RepositorioProductos : Archivos
+    public class RepositorioProductos : ArchivosProductos
     {
         public RepositorioProductos() : base()
         {
---
+++ Logica/FuncionesUsuario.cs
@@ -0,0 +1,41 @@
+﻿using Datos;
+using Entitades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logica
+{
+    public class FuncionesUsuario : IServiciosPersona<Usuario>
+    {
+        List<Usuario> usuarios;
+        RepositorioUsuario repositorioUsuario = new RepositorioUsuario();
+
+        public FuncionesUsuario()
+        {
+            usuarios = new List<Usuario>();
+            usuarios = repositorioUsuario.GetAll();
+        }
+        public string Agregar(Persona persona)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string Editar(Persona persona)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string Eliminar(Persona persona)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Usuario> GetAllUsuarios()
+        {
+            return repositorioUsuario.GetAll();
+        }
+    }
+}
